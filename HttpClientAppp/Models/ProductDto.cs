b4df96a4becc6f9aa2a1673257ed05f02c06b377
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-10-04 15:23:29 +0500

    Add project files.

---
+++ HttpClientAppp/Models/ProductDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientAppp.Models
+{
+    public class ProductDto
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public long? CategoryId { get; set; }
+    }
+}
+
---
+++ HttpClientAppp/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using HttpClientAppp;
+
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+
+
+CrudMethods http = new CrudMethods();
+string result;
+//result = await http.GetAllAsync();
+
+//await http.PostAsync("Kitchen");
+//await http.GetAllAsync();
+await http.PutAsync(3, "Garment");
+//result = await http.PostAsync();
+//result = await http.DeleteAsync(4)
+
+
