eb5950cc532a1dcfacce211dc94e6d36b4ce6405
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-10-04 15:24:37 +0500

    something

--- HttpClientAppp/Program.cs
+++ HttpClientAppp/Program.cs
@@ -10,10 +10,11 @@ CrudMethods http = new CrudMethods();
 string result;
 //result = await http.GetAllAsync();
 
+Console.WriteLine();
+
 //await http.PostAsync("Kitchen");
 //await http.GetAllAsync();
 await http.PutAsync(3, "Garment");
 //result = await http.PostAsync();
 //result = await http.DeleteAsync(4)
-
 
